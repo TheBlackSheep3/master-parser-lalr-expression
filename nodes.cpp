9e1f03a295b58b16f083f34964bc77efa9f6248f
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2024-05-27 13:21:25 +0200

    printing notes output looks like python example from lecture

--- nodes.cpp
+++ nodes.cpp
@@ -7,31 +7,31 @@ std::ostream &operator<<(std::ostream &ostream,
   return ostream;
 }
 std::string nd::Expression::Tree::AdditionExpression::get_string() const {
-  return std::string("add(" + m_term->get_string() + "," +
+  return std::string("E(" + m_term->get_string() + "," +
                      m_expression->get_string() + ")");
 }
 
 std::string nd::Expression::Tree::TermExpression::get_string() const {
-  return m_term->get_string();
+  return std::string("E(" + m_term->get_string() + ")");
 }
 
 std::string nd::Expression::Tree::MultiplicationTerm::get_string() const {
-  return std::string("mul(" + m_factor->get_string() + "," +
+  return std::string("T(" + m_factor->get_string() + "," +
                      m_term->get_string() + ")");
 }
 
 std::string nd::Expression::Tree::FactorTerm::get_string() const {
-  return m_factor->get_string();
+  return std::string("T(" + m_factor->get_string() + ")");
 }
 
 std::string nd::Expression::Tree::Literal::get_string() const {
-  return std::string("Lit=" + std::to_string(m_value));
+  return std::to_string(m_value);
 }
 
 std::string nd::Expression::Tree::Identifier::get_string() const {
-  return std::string("Id=" + m_id);
+  return m_id;
 }
 
 std::string nd::Expression::Tree::ParenthesisExpression::get_string() const {
-  return std::string("Parenthesis(" + m_exp->get_string() + ")");
+  return m_exp->get_string();
 }
